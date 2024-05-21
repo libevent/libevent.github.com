95fbf562c2e794c05869767f6025211ad284e9af
emma-coronado <emma.dz.coronado@gmail.com>
2024-05-21 11:17:04 -0400

    start on downloads script

--- scripts/download.js
+++ scripts/download.js
@@ -1,1 +1,25 @@
+var StableNavItems = '';
+var StableCards = '';
 
+var RCNavItems = '';
+var RCCards = '';
+
+var AlphaNavItems = '';
+var AlphaCards = '';
+
+var OlderNavItems = '';
+var OlderCards = '';
+
+async function loadJson(){
+    const temp = await fetch("content/releases.json");
+    const releasesInfo = await temp.json();
+    console.log(releasesInfo);
+}
+
+loadJson();
+/*
+document.getElementById("stable-nav").innerHTML = StableNavItems;
+document.getElementById("rc-nav").innerHTML = RCNavItems;
+document.getElementById("alpha-nav").innerHTML = AlphaNavItems;
+document.getElementById("older-nav").innerHTML = OlderNavItems;
+*/
