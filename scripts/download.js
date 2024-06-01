7c83a4a1a8e9d70380a0ed669470c7828b1be933
emma-coronado <emma.dz.coronado@gmail.com>
2024-06-01 12:52:18 -0400

    continue working on downloads page

--- scripts/download.js
+++ scripts/download.js
@@ -1,3 +1,15 @@
+async function processJson(){
+    const temp = await fetch("https://raw.githubusercontent.com/emma-coronado/libevent.github.com/master/content/releases.json");
+    const releasesInfo = await temp.json();
+    return releasesInfo;
+}
+
+function buildCard(release){
+    let card = '';
+
+    return card;
+}
+
 var StableNavItems = '';
 var StableCards = '';
 
@@ -10,16 +22,11 @@ var AlphaCards = '';
 var OlderNavItems = '';
 var OlderCards = '';
 
-async function loadJson(){
-    const temp = await fetch("content/releases.json");
-    const releasesInfo = await temp.json();
-    console.log(releasesInfo);
-}
+const temp = processJson();
+console.log(temp);
 
-loadJson();
-/*
+
 document.getElementById("stable-nav").innerHTML = StableNavItems;
 document.getElementById("rc-nav").innerHTML = RCNavItems;
 document.getElementById("alpha-nav").innerHTML = AlphaNavItems;
 document.getElementById("older-nav").innerHTML = OlderNavItems;
-*/
